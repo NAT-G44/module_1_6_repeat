0c3ae0b2bb3ba576dc2ceebe379bd5be941d8444
NAT-G44 <gr.natasha.2003@gmail.com>
2024-10-29 16:36:57 +0800

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,6 +1,6 @@
 all_homework = 12
 all_hours = 1.5
 name_course = "Pyton,"
-time_one = float(float(all_hours)/(all_homework))
+time_one = float((all_hours)/(all_homework))
 print("Курс:",name_course,"всего задач:",all_homework,"затрачено часов:",all_hours,"среднее время выполнения",
       time_one,"часа")
